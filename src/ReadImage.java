cfa1be18820f395b49e5d63aaf0f2e19756b7ccb
NashwaEl <elkha22n@mtholyoke.edu>
2015-04-21 20:54:17 -0400

    nachwa's edits

--- src/ReadImage.java
+++ src/ReadImage.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -6,55 +7,22 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class ReadImage {
-	
-	int[] chars = new int[255];
-	String str ="";
-	
-	public void readColors(){
-		
-    BufferedImage bi;
-	int countloop=0;
-	try {
-		bi = ImageIO.read(new File("test.png")); 
-		
-		for (int x = 0; x <bi.getWidth(); x++) {
-		    for (int y = 0; y < bi.getHeight(); y++) {
-		        Color c = new Color(bi.getRGB(x, y));
-		        System.out.println("red=="+c.getRed()+" green=="+c.getGreen()+"    blue=="+c.getBlue()+"  countloop="+countloop++);      
-		        System.out.println("y"+ y);
-		        
-		        //&& (c.getGreen()!=0 || c.getGreen()!=255 )&& (c.getBlue()!=0 || c.getBlue()!=255)
-				if( c.getRed()!=0 && c.getRed()!=255 && c.getGreen()!=0 && c.getGreen()!=255 && c.getBlue()!=0 && c.getBlue()!=255  ){
-					
-					chars[c.getBlue()] = c.getRed();
 
-				}
-				
-		    }
-		   
-		}
-
-	} catch (IOException e) {
-		
-		//e.printStackTrace();
-		System.out.print("Image not found");
-	}
-
-}
-	public String getCharValue(){
-
-      for(int i= 0; i<chars.length;i++){
-    	  
-    	  str = str+ (char)chars[i];
-      }
-		return str;
-	}
 	
 	public static void main(String[] args){
-		ReadImage readImage = new ReadImage();
-		readImage.readColors();
-		System.out.println(readImage.getCharValue());
+	
 
+		Chunkify chunkified1 = new Chunkify(0,50);
+		Chunkify chunkified2 = new Chunkify(50,100);
+		Chunkify chunkified3 = new Chunkify(100,150);
+		Chunkify chunkified4 = new Chunkify(150,200);
+		
+		chunkified1.run();
+		chunkified2.run();
+		chunkified3.run();
+		chunkified4.run();
+		
+		System.out.println("chars "+chunkified1.getCharValue());
 		
 	}
 }
