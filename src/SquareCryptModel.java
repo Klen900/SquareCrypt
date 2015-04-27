d9bea207086785246033b9264df82ff8bd2a0acd
NashwaEl <elkha22n@mtholyoke.edu>
2015-04-27 19:03:08 -0400

    nachwa's edits

--- src/SquareCryptModel.java
+++ src/SquareCryptModel.java
@@ -1,11 +1,17 @@
 public class SquareCryptModel {
-	
+	/**
+	 * This class creates the matrix that holds the character values
+	 * The rows represent the numerical value of the character that goes from 0-127
+	 * The columns hold the frequency of each character which can get to 255
+	 * We first create an empty matrix with values of -1. the values that the matrix holds is the order at which the character was entered
+	 * 
+	 */
 	
 	//instance variables
 	int[][] matrix;
 	int row= 127;
 	int col = 255; 
-	int count = 1;
+	int count = 1;//counts the order 
 
 	Boolean isFull = false;
 
@@ -31,12 +37,14 @@ public class SquareCryptModel {
 		
 	}
 	
+	//erases all values in the matrix
 	public void clearAll(){
 		
 		 makeNewModel();
 		
 	}
 	
+	//checks if the matrix has reached 255 characters.
 	public Boolean isFull(int indX){
 		
 		if(count ==255){
@@ -48,6 +56,11 @@ public class SquareCryptModel {
 		
 	}
 	
+	/**
+	 * stores the char value in the matrix
+	 * @param str
+	 * @return
+	 */
 	public int[][] setMatrixIndecies(String str){
 
 		char chr;
@@ -57,29 +70,29 @@ public class SquareCryptModel {
 			
 			chr = str.charAt(k);
 			
-			x =  (int)chr;
+			x = (int)chr; //x is equal to the numerical value of the character
 			
-			y = (int)Math.floor(Math.random()*col);
+			y = (int)Math.floor(Math.random()*col); //y is a random value at the row in which the char is stored
 			
 			
-			//for duplicates
+			//for duplicated characters, we need to check if the column is full at each given row
 			if(!isFull(x)){
 				
             while(matrix[x][y] != -1){
             	
-            	y = (int)Math.floor(Math.random()*col);
+            	y = (int)Math.floor(Math.random()*col);//while the index at row = x is full, keep looking for another y value in which to store the char order
             	
 			}
 			}else{
 				
-				isFull = true;
-				count = 1;
+				isFull = true;//if the whole row is full, it means that we stored 255 chars, so the matrix returns true for isFull
+				count = 1;//set the count back to 1 for the new matrix
 
 			}
 			
-			matrix[x][y] = count;
+			matrix[x][y] = count; //when y is selected, store the order of the char in the matrix indices
 			
-			count ++;
+			count ++;//increment the count each time we do.
 			
 		}
 		return matrix;
